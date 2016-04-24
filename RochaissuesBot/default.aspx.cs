ef5dcf868bc39703cb8d6c0c1cd9de7efe6100f4
sebagomez <sebagomezcorrea@outlook.com>
2016-04-24 16:10:54 -0300

    aspx added with github ribon and version in the footer

---
+++ RochaissuesBot/default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+using System.Web.UI;
+
+namespace GXIssueTrackingBot
+{
+	public partial class _default : System.Web.UI.Page
+	{
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (!IsPostBack)
+			{
+				Assembly assembly = Assembly.GetExecutingAssembly();
+				AssemblyName name = assembly.GetName();
+				Version ver = name.Version;
+
+				Page.Title = $"Genexus Issue Tracking bot v{ver.Major}.{ver.Minor}.{ver.Build}.{ver.Revision}";
+				lblVersion.Text = ver.ToString();
+			}
+		}
+	}
+}
